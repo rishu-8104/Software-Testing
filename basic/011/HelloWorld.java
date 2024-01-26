4f161c0b029a2ef5a00ee95d2fb93d2d91dfaa29
Rishu Kumar <rishu.kumar@tuni.fi>
2024-01-26 12:57:26 +0200

    basic/011: add Javadoc to class

--- basic/011/HelloWorld.java
+++ basic/011/HelloWorld.java
@@ -1,5 +1,10 @@
+/**
+ * @author      Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version     2024.0126 (last modified)
+ * @since       17.0 (miminimum Java version)
+ */
 class HelloWorld{
-		public static void hello() {
+	public static void hello() {
         System.out.println("Hello World");
     }
 
