05626601899ba947fb48b7d404e9ef53395081fe
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-06 17:25:28 +0200

    added java doc

--- basic/012/PrintProgram.java
+++ basic/012/PrintProgram.java
@@ -1,20 +1,21 @@
 /**
- * @author      Rishu Kumar <Rishu.kumar@tuni.fi>
- * @version     2024.0131 (last modified)
- * @since       17.0 (miminimum Java version)
+ * The PrintProgram class demonstrates printing information.
+ *
+ * @author Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version 2024.0131 (last modified)
+ * @since 17.0 (minimum Java version)
  */
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PrintProgram {
-     /**
+    /**
      * Exercise: 1.2
      * Write the Print program.
      *
-     * @param  args  Command line arguments. Not used.
+     * @param args Command line arguments. Not used.
      */
     public static void main(String[] args) {
-
         PrintProgram printer = new PrintProgram();
 
         String currentDate = getCurrentDate();
@@ -25,6 +26,15 @@ public class PrintProgram {
         System.out.println(output);
     }
 
+    /**
+     * Display the provided information.
+     *
+     * @param currentDate The current date.
+     * @param firstName   The first name.
+     * @param lastName    The last name.
+     * @param height      The height.
+     * @return A string representing the displayed information.
+     */
     public String displayInformation(String currentDate, String firstName, String lastName, int height) {
         StringBuilder output = new StringBuilder();
         output.append("The date is ").append(currentDate).append("\n");
@@ -33,12 +43,13 @@ public class PrintProgram {
         return output.toString();
     }
 
+    /**
+     * Get the current date in the format "yyyy-MM-dd".
+     *
+     * @return The current date.
+     */
     public static String getCurrentDate() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         return dateFormat.format(new Date());
     }
 }
-
-
-// End of file
-
