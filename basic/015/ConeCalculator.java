ca719400f63f5219cfc1f1e1f5b6bb4d9aacd641
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-06 17:28:27 +0200

    added java doc

--- basic/015/ConeCalculator.java
+++ basic/015/ConeCalculator.java
@@ -1,14 +1,17 @@
 /**
- * @author      Rishu Kumar <Rishu.kumar@tuni.fi>
- * @version     2024.0131 (last modified)
- * @since       17.0 (miminimum Java version)
+ * The ConeCalculator class calculates and displays the volume of a cone.
+ * It provides methods to calculate the volume of a cone and display the results.
+ *
+ * @author Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version 2024.0131 (last modified)
+ * @since 17.0 (minimum Java version)
  */
 public class ConeCalculator {
-/**
+    /**
      * Exercise: 1.5
      * Write the cone calculator program.
      *
-     * @param  args  Command line arguments. Not used.
+     * @param args Command line arguments. Not used.
      */
     public static void main(String[] args) {
         ConeCalculator calculator = new ConeCalculator();
@@ -16,20 +19,38 @@ public class ConeCalculator {
         System.out.println(result);
     }
 
+    /**
+     * Calculates the volume of a cone given its radius and height.
+     *
+     * @param radius The radius of the cone.
+     * @param height The height of the cone.
+     * @return The volume of the cone.
+     */
+    public double calculateConeVolume(double radius, double height) {
+        return (Math.PI * radius * radius * height) / 3;
+    }
+
+    /**
+     * Calculates the volume of a cone and displays the results.
+     *
+     * @param radius The radius of the cone.
+     * @param height The height of the cone.
+     * @return A string representing the cone volume calculation results.
+     */
     public String calculateAndDisplayConeVolume(double radius, double height) {
         double volume = calculateConeVolume(radius, height);
         return displayResults(radius, height, volume);
     }
 
-    public double calculateConeVolume(double radius, double height) {
-        return (Math.PI * radius * radius * height) / 3;
-    }
-
+    /**
+     * Formats and displays the cone volume calculation results.
+     *
+     * @param radius The radius of the cone.
+     * @param height The height of the cone.
+     * @param volume The volume of the cone.
+     * @return A string representing the formatted cone volume calculation results.
+     */
     public String displayResults(double radius, double height, double volume) {
         return String.format("Radius (r): %.2f\nHeight (h): %.2f\nVolume    : %.2f", radius, height, volume);
     }
 }
-
-
-// End of file
-
