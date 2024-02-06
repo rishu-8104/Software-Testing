0aa003a9afabf5c51d61e76634daf1eb356a67c5
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-06 17:21:03 +0200

    added java doc to helloworld

--- basic/011/HelloWorld.java
+++ basic/011/HelloWorld.java
@@ -1,20 +1,26 @@
 /**
  * @author      Rishu Kumar <Rishu.kumar@tuni.fi>
  * @version     2024.0126 (last modified)
- * @since       17.0 (miminimum Java version)
+ * @since       17.0 (minimum Java version)
  */
-class HelloWorld{
-/**
+class HelloWorld {
+    /**
      * Exercise: 1.1
      * Write the Hello World program.
      *
-     * @param  args  Command line arguments. Not used.
+     * @param args Command line arguments. Not used.
      */
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         String formattedBox = getFormattedNameBox("Rishu Kumar");
         System.out.println(formattedBox);
     }
 
+    /**
+     * Generates a formatted box containing the provided name.
+     *
+     * @param name The name to be included in the box.
+     * @return A string representing the formatted name box.
+     */
     public static String getFormattedNameBox(String name) {
         int length = name.length() + 16; // Considering padding and asterisks
 
@@ -22,7 +28,5 @@ class HelloWorld{
 
         return String.format("%s\n* Hello, I am %s *\n%s\n", border, name, border);
     }
-
 }
 
-// End of file
