105f753760db466453668ccf978c3352ac17bc34
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-06 17:27:08 +0200

    added java doc

--- basic/013/BMICalculator.java
+++ basic/013/BMICalculator.java
@@ -1,15 +1,16 @@
 /**
- * @author      Rishu Kumar <Rishu.kumar@tuni.fi>
- * @version     2024.0131 (last modified)
- * @since       17.0 (miminimum Java version)
+ * The BMICalculator class calculates and displays the BMI (Body Mass Index).
+ *
+ * @author Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version 2024.0131 (last modified)
+ * @since 17.0 (minimum Java version)
  */
-
 public class BMICalculator {
-/**
+    /**
      * Exercise: 1.3
      * Write the BMI calculator program.
      *
-     * @param  args  Command line arguments. Not used.
+     * @param args Command line arguments. Not used.
      */
     public static void main(String[] args) {
         BMICalculator calculator = new BMICalculator();
@@ -17,15 +18,25 @@ public class BMICalculator {
         System.out.println(result);
     }
 
-    public String calculateAndDisplayBMI(double weight, double height) {
-        double bmi = calculateBMI(weight, height);
-        return displayResults(weight, height, bmi);
-    }
-
+    /**
+     * Calculates the BMI based on weight and height.
+     *
+     * @param weight The weight in kilograms.
+     * @param height The height in meters.
+     * @return The calculated BMI.
+     */
     public double calculateBMI(double weight, double height) {
         return weight / (height * height);
     }
 
+    /**
+     * Displays the BMI calculation results.
+     *
+     * @param weight The weight in kilograms.
+     * @param height The height in meters.
+     * @param bmi    The calculated BMI.
+     * @return A string representing the BMI calculation results.
+     */
     public String displayResults(double weight, double height, double bmi) {
         StringBuilder result = new StringBuilder();
         result.append(String.format("Weight (kg): %.2f\n", weight));
@@ -33,6 +44,16 @@ public class BMICalculator {
         result.append(String.format("BMI       : %.2f\n", bmi));
         return result.toString();
     }
-}
 
-// End of file
+    /**
+     * Calculates the BMI based on weight and height, and displays the results.
+     *
+     * @param weight The weight in kilograms.
+     * @param height The height in meters.
+     * @return A string representing the BMI calculation results.
+     */
+    public String calculateAndDisplayBMI(double weight, double height) {
+        double bmi = calculateBMI(weight, height);
+        return displayResults(weight, height, bmi);
+    }
+}
