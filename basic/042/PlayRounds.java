4717f902385c5ff81d9c2c908c6711e710379ce0
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-08 21:33:41 +0200

    adding exercise 042

---
+++ basic/042/PlayRounds.java
@@ -0,0 +1,34 @@
+/**
+ * The PlayRounds class determines how many rounds a player can afford to play until running out of money.
+ * It provides a method to calculate the number of rounds.
+ *
+ * @author Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version 2024.0131 (last modified)
+ * @since 17.0 (minimum Java version)
+ */
+public class PlayRounds {
+
+    /**
+     * Determines the number of rounds a player can afford to play until running out of money.
+     *
+     * @param money      The amount of money the player has.
+     * @param roundPrice The price of each round.
+     * @return The number of rounds the player can afford to play.
+     */
+    public double play(double money, double roundPrice) {
+        return money / roundPrice;
+    }
+
+    /**
+     * Main method to run the program.
+     *
+     * @param args Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        double money = 20.55;
+        double roundPrice = 1.5;
+        PlayRounds playRounds = new PlayRounds();
+        double rounds = playRounds.play(money, roundPrice);
+        System.out.println("Number of rounds: " + rounds);
+    }
+}
