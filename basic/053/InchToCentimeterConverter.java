b700c1c73a3222210da5b77e7c95d3aff85d0d6d
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-09 11:19:19 +0200

    adding exercise 053

---
+++ basic/053/InchToCentimeterConverter.java
@@ -0,0 +1,52 @@
+/**
+ * The InchToCentimeterConverter class converts an array of inch values to centimeters.
+ * It then displays the conversions.
+ *
+ * @author Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version 2024.0131 (last modified)
+ * @since 17.0 (minimum Java version)
+ */
+public class InchToCentimeterConverter {
+    private double[] inches;
+    private double[] centimeters;
+
+    /**
+     * Constructs an InchToCentimeterConverter object with the given inch values.
+     *
+     * @param inches The array of inch values.
+     */
+    public InchToCentimeterConverter(double[] inches) {
+        this.inches = inches;
+        this.centimeters = new double[inches.length];
+        convertToCentimeters();
+    }
+
+    /**
+     * Converts the inch values to centimeters.
+     */
+    private void convertToCentimeters() {
+        for (int i = 0; i < inches.length; i++) {
+            centimeters[i] = inches[i] * 2.54;
+        }
+    }
+
+    /**
+     * Displays the conversions from inches to centimeters.
+     */
+    public void displayConversions() {
+        for (int i = 0; i < inches.length; i++) {
+            System.out.printf("%.1f in = %.2f cm%n", inches[i], centimeters[i]);
+        }
+    }
+
+    /**
+     * Main method to run the program.
+     *
+     * @param args Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        double[] inches = {1, 2, 3};
+        InchToCentimeterConverter converter = new InchToCentimeterConverter(inches);
+        converter.displayConversions();
+    }
+}
