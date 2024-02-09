0e4c2202de0a667cd6427cb38e7837f24c54cf12
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-09 11:17:27 +0200

    adding exercise 043

---
+++ basic/043/TaxCalculator.java
@@ -0,0 +1,34 @@
+/**
+ * The TaxCalculator class calculates taxes based on the yearly salary and tax percentage.
+ * It provides a method to calculate yearly taxes.
+ *
+ * @author Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version 2024.0131 (last modified)
+ * @since 17.0 (minimum Java version)
+ */
+public class TaxCalculator {
+
+    /**
+     * Calculates yearly taxes based on the yearly salary and tax percentage.
+     *
+     * @param yearlySalary The yearly salary of the individual.
+     * @param percentage   The tax percentage to be applied.
+     * @return The amount of taxes to be paid yearly.
+     */
+    public double yearlyTaxes(double yearlySalary, double percentage) {
+        return yearlySalary * (percentage / 100.0);
+    }
+
+    /**
+     * Main method to run the program.
+     *
+     * @param args Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        double yearlySalary = 50000.0;
+        double percentage = 25.0;
+        TaxCalculator taxCalculator = new TaxCalculator();
+        double taxes = taxCalculator.yearlyTaxes(yearlySalary, percentage);
+        System.out.println("Yearly taxes: " + taxes);
+    }
+}
