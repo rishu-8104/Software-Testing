334afc6c7d77f8da25150038a646687415de9866
Rishu Kumar <rishu.kumar@tuni.fi>
2024-02-14 15:04:31 +0200

    modifing doc

--- basic/011/HelloWorld.java
+++ basic/011/HelloWorld.java
@@ -1,24 +1,33 @@
 /**
- * @author      Rishu Kumar <Rishu.kumar@tuni.fi>
- * @version     2024.0126 (last modified)
- * @since       17.0 (minimum Java version)
+ * Represents a simple program to print a formatted box containing a name.
+ *
+ * @author Rishu Kumar <Rishu.kumar@tuni.fi>
+ * @version 2024.0126 (last modified)
+ * @since 17.0 (minimum Java version)
  */
 public class HelloWorld {
 
+    /**
+     * Executes the program.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
-        HelloWorld helloWorld = new HelloWorld(); // Instantiating the HelloWorld object
-        helloWorld.printFormattedNameBox("Rishu Kumar");
+        HelloWorld helloWorld = new HelloWorld();
+        String formattedBox = helloWorld.printFormattedNameBox("Rishu Kumar");
+        System.out.println(formattedBox); // Print the formatted box
     }
 
     /**
-     * Exercise: 1.1
-     * Write the Hello World program.
+     * Exercise: 1.1 Write the Hello World program.
+     *
+     * Prints a formatted box containing the provided name.
      *
      * @param name The name to be included in the box.
+     * @return A string representing the formatted name box.
      */
-    public void printFormattedNameBox(String name) {
-        String formattedBox = getFormattedNameBox(name);
-        System.out.println(formattedBox);
+    public String printFormattedNameBox(String name) {
+        return getFormattedNameBox(name);
     }
 
     /**
