230f2832ddf83d0d51fccc5b9f62dd3fd279f382
Rishu Kumar <rishu.kumar@tuni.fi>
2024-03-26 03:30:52 +0200

    adding Main.java

---
+++ project/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        LibrarySystem librarySystem = new LibrarySystem();
+        librarySystem.initializeSystem();
+
+
+        librarySystem.shutdownSystem();
+    }
+}
